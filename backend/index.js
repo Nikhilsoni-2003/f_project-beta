c782c40e34dee3d6655eda6a7bc6d17027835efd
Tonmoy Mukherjee <73432344+tonmoy-cpu@users.noreply.github.com>
2025-08-29 13:40:21 +0000

    ggg

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config()
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"https://turbo-winner-g6gq5rw664pcp5x-5173.app.github.dev/https://turbo-winner-g6gq5rw664pcp5x-5173.app.github.dev",
+    origin:"https://turbo-winner-g6gq5rw664pcp5x-5173.app.github.dev",
     credentials:true
 }))
 app.use(express.json())
