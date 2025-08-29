d1424f0f9e09f7bb8a4e0cb47fcf53e16d46efe8
Tonmoy Mukherjee <73432344+tonmoy-cpu@users.noreply.github.com>
2025-08-29 13:45:26 +0000

    rollback commit

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config()
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"https://turbo-winner-g6gq5rw664pcp5x-5173.app.github.dev",
+    origin:"http://localhost:5173",
     credentials:true
 }))
 app.use(express.json())
