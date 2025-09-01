53b3c800218aa37830e96b36c630a8aac5fb010b
Nikhil Soni <niksoni619@gmail.com>
2025-09-02 03:28:39 +0530

    preflight response fixed

--- aws-backend/src/functions/auth/index.js
+++ aws-backend/src/functions/auth/index.js
@@ -6,6 +6,18 @@ const { createSuccessResponse, createErrorResponse } = require('../../utils/resp
 exports.handler = async (event) => {
   const { httpMethod, path, body } = event;
   const parsedBody = body ? JSON.parse(body) : {};
+  
+  if (httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+      },
+      body: JSON.stringify({ message: 'CORS preflight OK' })
+    };
+  }
 
   try {
     switch (true) {
